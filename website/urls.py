22ac7ff3ad0f3dadbbdff52cba8ec6ff64816b28
Yuva <yuvasaiteja25@gmail.com>
2025-01-09 17:15:41 +0530

    Your commit message

--- website/urls.py
+++ website/urls.py
@@ -5,6 +5,6 @@ from website import views
 
 urlpatterns = [
     path('',views.home,name='home'),
-    path('login',views.login_user,name='login'),
+    path('register/',views.register_user,name='register'),
     path('logout',views.logout_user,name='logout')
 ]
